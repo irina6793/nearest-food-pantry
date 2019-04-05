0bc05219a7927ce98f444238986f4fbac2a30ed3
Irina <irina6793@yahoo.com>
2019-04-05 12:12:54 -0400

    first

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,10 +1,32 @@
 const path = require("path");
 const viewsFolder = path.join(__dirname, "..", "views");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const flash = require("express-flash");
+const passportConfig = require("./passport-config");
+const session = require("express-session");
 
 module.exports = {
   init(app, express) {
     app.set("views", viewsFolder);
     app.set("view engine", "ejs");
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(
+      session({
+        secret: process.env.cookieSecret,
+        resave: false,
+        saveUninitialized: false,
+        cookie: { maxAge: 1.21e9 } //set cookie to expire in 14 days
+      })
+    );
+    app.use(flash());
+    passportConfig.init(app);
+    app.use((req, res, next) => {
+      res.locals.currentUser = req.user;
+      next();
+    });
+
     app.use(express.static(path.join(__dirname, "..", "assets")));
+    app.use(logger("dev"));
   }
 };
