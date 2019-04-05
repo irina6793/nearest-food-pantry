5c20abe81e20aa5171729709364df20824ebedc7
Irina <irina6793@yahoo.com>
2019-04-04 21:17:50 -0400

    first

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,6 @@
 const userQueries = require("../db/queries.users.js");
+const passport = require("passport");
+const sgMail = require("@sendgrid/mail");
 const express = require("express");
 const router = express.Router();
 
@@ -15,5 +17,27 @@ module.exports = {
       passwordConfirmation: req.body.passwordConfirmation,
       zipcode: req.body.zipcode
     };
+    userQueries.createUser(newUser, (err, user) => {
+      if (err) {
+        console.log(err);
+        req.flash("error", err);
+        res.redirect("/user/signup");
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          req.flash("notice", "You've successfully signed in!");
+          sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+          const msg = {
+            to: "irina6793@yahoo.com",
+            from: "test@example.com",
+            subject: "Sending with SendGrid is Fun",
+            text: "and easy to do anywhere, even with Node.js",
+            html: "<strong>and easy to do anywhere, even with Node.js</strong>"
+          };
+          console.log("Sending message...");
+          sgMail.send(msg);
+          res.redirect("/");
+        });
+      }
+    });
   }
 };
---
+++ src/db/migrations/20190405001547-create-user.js
@@ -0,0 +1,38 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: { isEmail: { msg: "must be a valid email" } },
+        unique: true
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Users");
+  }
+};
