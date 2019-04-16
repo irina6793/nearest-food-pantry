82969dc340d8f74e3e410379b09d1f9f62494718
Irina <irina6793@yahoo.com>
2019-04-16 09:04:52 -0400

    first

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,9 +1,26 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {});
+  var User = sequelize.define(
+    "User",
+    {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: { msg: "must be a valid email" }
+        }
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    {}
+  );
   User.associate = function(models) {
     // associations can be defined here
   };
